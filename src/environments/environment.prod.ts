b6c3c75057af7ded25b38df5c39d0406564c81cc
Israel Rodrigues de Jesus <israel.jesus2@zup.com.br>
2021-10-19 21:13:59 -0300

    adding Dockerfile and NGinx configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   redirectUrl: 'http://localhost:4200/',
   shortifyManagerUrl: 'http://localhost:8080/shortify-manager/shortify',
-  shortifySearchUrl: 'http://localhost:8081/shortify-search/'
+  shortifySearchUrl: 'http://localhost:8081/shortify-search'
 };
